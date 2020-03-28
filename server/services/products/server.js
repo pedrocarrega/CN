c7342d99898f0428c1f1c5d35fb16beff3b53f87
Vasco <vascobferreira98@gmail.com>
2020-03-28 16:02:52 +0000

    formato updated para microservices -Falta adaptar o deploy.sh -Nao sei quanto aos dockerfiles -rule.json foram copiados do exemplo -terminar o product/salePrice

--- server/services/products/server.js
+++ server/services/products/server.js
@@ -1,30 +1,36 @@
-'use strict';
-const db = require('./db.json'); //to remove later
-//list categories
-var non_null = db.entries.filter((evt) => evt.category_code != "");
-var uniques = [...new Set(non_null.map(item => item.category_code))];
+const app = require('koa')();
+const router = require('koa-router')();
+const db = require('./db.json');
+
+// Log requests
+app.use(function *(next){
+  const start = new Date;
+  yield next;
+  const ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
 
 /**
  * Lists all product categories made available in the dataset
  *
  * returns List
  **/
-exports.listCategories = function() {
-  var non_null = db.entries.filter((evt) => evt.category_code != "");
-  var uniques = [...new Set(non_null.map(item => item.category_code))];
-}
 
+router.get('/api/products/listCategories', function *(next) {
+  var non_null = db.entries.filter((evt) => evt.category_code != "");
+  this.body = [...new Set(non_null.map(item => item.category_code))];
+});
 
 /**
  * Lists all brands in the dataset by popularity within all possible event types (view, cart and purchase)
  *
  * returns List
  **/
-exports.mostPopularBrands = function() {
+router.get('/api/products/mostPopularBrands', function *(next) {
   var sales = db.entries.filter((entry) => entry.brand != "").map(a => a.brand);
   sales.sort();
 
-  var result = foo(sales);
+  this.body = foo(sales);
 
   function foo(arr) {
     var prev;
@@ -41,7 +47,8 @@ exports.mostPopularBrands = function() {
       }
       return conjunto;
   }
-}
+});
+
 
 
 /**
@@ -50,6 +57,18 @@ exports.mostPopularBrands = function() {
  * brand String Brand name
  * returns List
  **/
+router.get('/api/products/salePrice', function *(next) {
+  const entries = db.entries.filter((entries) => entries.brand == brand);
+    
+    if (Object.keys(entries).length > 0) {
+
+      let result = [...new Set(entries.filter((object,index) => index === entries.findIndex(obj => JSON.stringify(obj.product_id) === JSON.stringify(object.product_id))).map(item => item.price))];
+
+      resolve(Math.round(((result.reduce(function(total, num){
+        return parseFloat(total) + parseFloat(num);
+      })/result.length) + Number.EPSILON) * 100) / 100);
+});
+/*
 exports.salePrice = function(brand) {
   return new Promise(function(resolve, reject) {
     
@@ -66,7 +85,7 @@ exports.salePrice = function(brand) {
       resolve(-1);
     }
   });
-}
+}*/
 
 
 /**
@@ -74,7 +93,13 @@ exports.salePrice = function(brand) {
  *
  * returns List
  **/
-exports.salesByBrand = function() {
-  var sales = db.entries.filter((entry) => entry.event_type == "purchase");
-}
+router.get('/api/products/salesByBrand', function *(next) {
+  this.body = db.entries.filter((entry) => entry.event_type == "purchase");
+});
 
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(3000);
+
+console.log('Worker started');
