784eb0362eefd9547d996cf9dd77a5109924792b
Vasco <vascobferreira98@gmail.com>
2020-03-31 11:36:59 +0100

    adicionei as queries no products mas ainda nao estao terminadas, estah apenas um esboco

--- server/services/products/server.js
+++ server/services/products/server.js
@@ -2,6 +2,20 @@ const app = require('koa')();
 const router = require('koa-router')();
 const db = require('./db.json');
 
+//inicio de utilizacao do dynamo
+
+var AWS = require("aws-sdk");
+//Mudar os credenciais aqui
+let awsConfig = {
+    "region": "us-east-1",
+    "endpoint": "http://dynamodb.us-east-1.amazonaws.com",
+    "accessKeyId": "ID", 
+    "secretAccessKey": "SECRET_KEY"
+};
+
+AWS.config.update(awsConfig);
+let docClient = new AWS.DynamoDB;
+
 // Log requests
 app.use(function *(next){
   const start = new Date;
@@ -17,6 +31,29 @@ app.use(function *(next){
  **/
 
 router.get('/api/products/listCategories', function *(next) {
+
+  var params = {
+    TableName: "DBTest",
+    ProjectionExpression: "category_code",
+    FilterExpression: "#cc != :''", //not sure, nao quero strings vazias
+    ExpressionAttributeNames: {
+      "#cc": "category_code",
+    }
+    /*Falta ainda selecionar os valores distinct
+    Uma solucao seria fazer aqui a selecao de distinct values, 
+    mas era melhor ver se a bd tem capacidade para o fazer*/
+  }
+
+  docClient.query (params, function (err, data) {
+    if (err) {
+        console.log("products::listCategories::error - " + JSON.stringify(err, null, 2));
+    }
+    else {
+        console.log("products::listCategories::success - " + JSON.stringify(data, null, 2));
+    }
+  })
+
+
   var non_null = db.entries.filter((evt) => evt.category_code != "");
   this.body = [...new Set(non_null.map(item => (item.category_code)))];
 });
@@ -27,6 +64,25 @@ router.get('/api/products/listCategories', function *(next) {
  * returns List
  **/
 router.get('/api/products/popularBrands', function *(next) {
+
+  var params = {
+    TableName: "DBTest",
+    ProjectionExpression: "brand",
+    FilterExpression: "#b != :''", //not sure, nao quero strings vazias
+    ExpressionAttributeNames: {
+      "#b": "brand",
+    }
+  }
+
+  docClient.query (params, function (err, data) {
+    if (err) {
+        console.log("products::popularBrands::error - " + JSON.stringify(err, null, 2));
+    }
+    else {
+        console.log("products::popularBrands::success - " + JSON.stringify(data, null, 2));
+    }
+  })
+
   var sales = db.entries.filter((entry) => entry.brand != "").map(a => a.brand);
 
   this.body = foo(sales);
@@ -57,6 +113,30 @@ router.get('/api/products/popularBrands', function *(next) {
  * returns List
  **/
 router.get('/api/products/salePrice', function *(next) {
+
+  var params = {
+    TableName: "DBTest",
+    ProjectionExpression: "price",
+    FilterExpression: "#b = :b_name",
+    ExpressionAttributeNames: {
+        "#b": "brand",
+    },
+    ExpressionAttributeValues: { 
+        ":b_name": {S: 'bq'}//aqui é que se dá filter à brand que vem como param?
+    }    
+}
+
+  docClient.query (params, function (err, data) {
+    if (err) {
+        console.log("products::popularBrands::error - " + JSON.stringify(err, null, 2));
+    }
+    else {
+        console.log("products::popularBrands::success - " + JSON.stringify(data, null, 2));
+    }
+  })
+
+  //depois de ir buscar todos os prices da brand, seguir a mesma logica
+
   const entries = db.entries.filter((entries) => entries.brand == brand && entries.event_type == "purchase");
     
   if (Object.keys(entries).length > 0) {
@@ -86,6 +166,29 @@ router.get('/api/products/salePrice', function *(next) {
  * returns List
  **/
 router.get('/api/products/salesByBrand', function *(next) {
+
+  var params = {
+    TableName: "DBTest",
+    ProjectionExpression: "brand",
+    FilterExpression: "#et = :evt_t and #b != :''", //posso fazer isto?
+    ExpressionAttributeNames: {
+        "#et": "event_type",
+        "#b": "brand"
+    },
+    ExpressionAttributeValues: { 
+        ":evt_t": {S: 'purchase'} //é assim que filtro so vendas?
+    }    
+  }
+
+  docClient.query (params, function (err, data) {
+    if (err) {
+        console.log("products::popularBrands::error - " + JSON.stringify(err, null, 2));
+    }
+    else {
+        console.log("products::popularBrands::success - " + JSON.stringify(data, null, 2));
+    }
+  })
+
   var sales = db.entries.filter((entry) => entry.brand != "" && entry.event_type == "purchase").map(a => a.brand);
 
   this.body = foo(sales);
