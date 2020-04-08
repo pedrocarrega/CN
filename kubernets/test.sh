1932cfc0be427ad0cce0dc1e27660858cc7f1170
Pedro Carrega <pedro.carrega@outlook.com>
2020-04-08 10:46:50 +0100

    add credentials file

--- kubernets/test.sh
+++ kubernets/test.sh
@@ -7,7 +7,7 @@ printf "CLUSTER NAME: ${CLUSTER_NAME}\n"
 printf "VPC STACK: ${STACK_NAME}\n"
 
 
-aws iam create-role --role-name eksServiceRole --assume-role-policy-document eks-service-role/assume-role.json --description "Allows EKS to manage clusters on your behalf."
+aws iam create-role --role-name eksServiceRole --assume-role-policy-document file://eks-service-role/assume-role.json --description "Allows EKS to manage clusters on your behalf."
 
 aws iam attach-role-policy --role-name eksServiceRole --policy-arn arn:aws:iam::aws:policy/AmazonEKSClusterPolicy
 aws iam attach-role-policy --role-name eksServiceRole --policy-arn arn:aws:iam::aws:policy/AmazonEKSServicePolicy
