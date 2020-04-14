cb106a85b1102772a7012b3d0b077c97fabc00be
CHKLE <ye.yang98@hotmail.com>
2020-04-14 21:59:33 +0100

    Removed temporary script files.

--- kubernets/temp_deploy2.sh
+++
@@ -1,8 +0,0 @@
-REGION=$1
-CLUSTER_NAME=$2
-
-kubectl apply -f ingress-test/ingress-rbac.yaml
-
-kubectl apply -f ingress-test/alb-ingress-controller.yaml
-
-kubectl apply -f ingress/api-ingress.yaml
