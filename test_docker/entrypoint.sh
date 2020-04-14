c2c2723ea88a55606282182a2ed9f0d4fc47e56f
Vasco-F <vascobferreira98@gmail.com>
2020-04-14 22:09:09 +0100

    ficheiro csv apaga-se depois de dar populate na bd

--- test_docker/entrypoint.sh
+++ test_docker/entrypoint.sh
@@ -2,4 +2,5 @@
 bash docker-entrypoint.sh mongod &
 sleep 10s
 mongoimport --db ecommerce --collection entries --type csv --file smallerLargeFile.csv --fields="event_time,event_type,product_id,category_id,category_code,brand,price,user_id,user_session"
+rm -f smallerLargeFile.csv
 wait
