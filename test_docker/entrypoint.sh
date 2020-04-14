be27899eb5c2081e8fa6655d146c80f520733db0
Vasco-F <vascobferreira98@gmail.com>
2020-04-14 21:25:32 +0100

    corrigi o cmd de import para ter os field corretos, ainda n sei se vai ser preciso o chmod no dockerfile

--- test_docker/entrypoint.sh
+++ test_docker/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 bash docker-entrypoint.sh mongod &
 sleep 10s
-mongoimport --db ecommerce --collection entries --type csv --file smallerLargeFile.csv --headerline
+mongoimport --db ecommerce --collection entries --type csv --file smallerLargeFile.csv --fields="event_time,event_type,product_id,category_id,category_code,brand,price,user_id,user_session"
 wait
