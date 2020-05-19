86b323f2250f4c6d8826686d0475fb9b2975ecda
Pedro Carrega <pedro.carrega@outlook.com>
2020-05-19 18:15:09 +0100

    fixing fuck up

--- service1.py
+++ service1.py
@@ -21,14 +21,15 @@ df = spark \
 	.option("header", "false") \
 	.csv("database/smallerLargeFile_3.csv")
 
-max_by_brand = df \
-		.select(col("_c3").alias("category_id"),col("_c4").alias("category_code"), col("_c5").alias("brand"), col("_c7").alias("user_id")) \
-		.filter(col("brand").isNotNull()) \
-		.groupBy("user_id", "category_id", "brand") \
+avg_by_view = df \
+		.select(col("_c1").alias("event_type"), col("_c8").alias("user_session") \
+		.filter(col("user_session").isNotNull()) \
+		.filter(col("event_type = purchase" | col("event_type = view"))) \
+		.groupBy("user_session", "event_type") \
 		.count() \
-		.groupBy("user_id", "category_id") \
 		.agg(max("count").alias("max"), sum("count").alias("total")) \
 		.agg(avg(col("max") / col("total"))).show()
-
+		
 spark.stop()
+
 
