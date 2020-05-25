6f3efdd5834f9324b366808ce72302cc17afc106
Vasco-F <vascobferreira98@gmail.com>
2020-05-25 11:39:29 +0100

    Updated spark deploy script to run the 2nd service, added sudo command to the auth and docker to work for every one

--- kubernets/spark-deploy/spark-deploy.sh
+++ kubernets/spark-deploy/spark-deploy.sh
@@ -1,6 +1,6 @@
-PROJECT_NAME='new-test-project-277912'
-ACCOUNT_NAME="test-account"
-BUCKET_NAME="cn-ecomm-test"
+PROJECT_NAME='logical-codex-275717'
+ACCOUNT_NAME="test-account43521112"
+BUCKET_NAME="cn-ecomm-test987"
 
 #Authenticates user
 gcloud auth login
@@ -20,22 +20,22 @@ gcloud services enable cloudbuild.googleapis.com
 
 #TODO UNCOMMENT THIS AFTER TESTING
 #creates a new owner account and the respective keyfile for authorization purposes
-#gcloud iam service-accounts create $ACCOUNT_NAME
-#gcloud projects add-iam-policy-binding $PROJECT_NAME--member "serviceAccount:${ACCOUNT_NAME}@${PROJECT_NAME}.iam.gserviceaccount.com" --role "roles/owner"
-#gcloud iam service-accounts keys create creds.json --iam-account $ACCOUNT_NAME@$PROJECT_NAME.iam.gserviceaccount.com
+gcloud iam service-accounts create $ACCOUNT_NAME
+gcloud projects add-iam-policy-binding $PROJECT_NAME --member "serviceAccount:${ACCOUNT_NAME}@${PROJECT_NAME}.iam.gserviceaccount.com" --role "roles/owner"
+gcloud iam service-accounts keys create creds.json --iam-account $ACCOUNT_NAME@$PROJECT_NAME.iam.gserviceaccount.com
 
 #create bucket
-#gsutil mb -p ${PROJECT_NAME} -l europe-west1 gs://$BUCKET_NAME/
+gsutil mb -p ${PROJECT_NAME} -l europe-west1 gs://$BUCKET_NAME/
 
 #These scripts generate the query files with the bucket name.
 #The file names will be Query1-3.py
-#./write-spark2.sh $BUCKET_NAME
+./write-spark2.sh $BUCKET_NAME
 #./write-spark1.sh $BUCKET_NAME
 #./write-spark3.sh $BUCKET_NAME
 
 #push pyspark queries to bucket
 #gsutil cp ./Query1.py gs://$BUCKET_NAME/
-#gsutil cp ./Query2.py gs://$BUCKET_NAME/
+gsutil cp ./Query2.py gs://$BUCKET_NAME/
 #gsutil cp ./Query3.py gs://$BUCKET_NAME/
 
 
@@ -54,7 +54,7 @@ gsutil cp gs://cn-ecommerce-container/spark-svc2.zip .
 unzip spark-svc2.zip
 #unzip spark-svc1.zip
 #unzip spark-svc3.zip
-#cp creds.json spark-svc2
+cp creds.json spark-svc2
 #cp creds.json spark-svc1
 #cp creds.json spark-svc3
 #rm -f creds.json
@@ -74,9 +74,10 @@ docker build -t gcr.io/$PROJECT_NAME/spark-svc2:v1 .
 cd ..
 rm -rf spark-svc2
 
-gcloud auth configure-docker
+#Pus sudo nestes dois comandos, o resto segui o que foi indicado
+sudo gcloud auth configure-docker
 
-docker push gcr.io/$PROJECT_NAME/spark-svc2:v1
+sudo docker push gcr.io/$PROJECT_NAME/spark-svc2:v1
 
 mkdir spark-svc2-kubernetes
 mkdir ingress-kubernetes
